66aa8a216e583c0af392c0fc7dcfa433b9d33151
MindHardt <71509337+MindHardt@users.noreply.github.com>
2021-05-24 12:46:44 +0500

    backend!!!!
    vot eto da

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+
+# -*- coding: utf-8 -*-
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('main page.html')
