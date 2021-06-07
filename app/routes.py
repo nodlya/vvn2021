02eeef1373477e213bcd3d5453dd9149dc8af4ac
MindHardt <71509337+MindHardt@users.noreply.github.com>
2021-06-07 12:00:53 +0500

    finalcut scary

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from app import app
+from flask_login import current_user, login_user
+from app.models import User
 
 # -*- coding: utf-8 -*-
 from flask import render_template
@@ -9,3 +11,21 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('main_page.html')
+
+@app.route(('/unauthorized'))
+def unauth():
+    return  render_template('meme_page.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return render_template('main_page.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
--- flaskProject1/templates/registration.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('login', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    name = StringField('name', validators=[DataRequired()])
-    submit = SubmitField('submit')
